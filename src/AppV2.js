ff113c16da0a4ec1b3c3f7d6b1eed1cd5763d73f
root <root@tzaf-kali.kali-home.com>
2023-10-31 20:02:43 +0200

    first push lats go

---
+++ src/AppV2.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react';
+
+
+
+export default function AppV2(){
+
+    return(
+        <div>
+                <h1>testing serverless functions</h1>
+        </div>
+    )
+}
