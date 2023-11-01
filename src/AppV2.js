1c590bfb99431bae6c3f8aeba8aa155b056618f6
root <root@tzaf-kali.kali-home.com>
2023-11-01 18:23:18 +0200

    testing API with react

--- src/AppV2.js
+++ src/AppV2.js
@@ -5,9 +5,16 @@ import React, { Component } from 'react';
 
 export default function AppV2(){
 
+async function onGetData(){
+    console.log("start")
+    const server_response = await fetch('/.netlify/functions/testing')
+    console.log(" the response : ",server_response)
+}    
+
     return(
         <div>
                 <h1>testing serverless functions</h1>
+                <button style={{width: "200px" , height: "100px" , fontSize:"38px" , fontWeight:"800"}} onClick={()=> onGetData ( console.log("button"))}>click me</button>
         </div>
     )
 }
